ea4f0e9874791cc40bc2b7081a500f8069db00f3
shuck. <valentin.sterler@gmail.com>
2026-05-25 08:53:12 +0200

    Load language JSON and replace tokens
    Enable simple localization: add .gitignore entries for language files, change list fetch to jstranslate_langs.txt, and add getLanguageJSON + logic in main() to load the selected language JSON and replace token keys in the DOM via replaceTextEverywhere. Also update test HTML to use token placeholders (:greeting:, :description:). Minor formatting/indentation cleanup and temporarily keep list parsing commented out.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function replaceTextEverywhere(searchText, replaceText) {
-    const walker = document.createTreeWalker(
-        document.body,
-        NodeFilter.SHOW_TEXT
-    );
+  const walker = document.createTreeWalker(
+    document.body,
+    NodeFilter.SHOW_TEXT
+  );
 
-    let node;
+  let node;
 
-    while ((node = walker.nextNode())) {
-        node.nodeValue = node.nodeValue.replaceAll(searchText, replaceText);
-    }
+  while ((node = walker.nextNode())) {
+    node.nodeValue = node.nodeValue.replaceAll(searchText, replaceText);
+  }
 }
 
 const language = navigator.language;
@@ -16,22 +16,30 @@ const language = navigator.language;
 console.log(language);
 
 async function loadList() {
-  const response = await fetch("/list.txt");
+  const response = await fetch("./jstranslate_langs.txt");
   const text = await response.text();
 
-  return text.split("\n");
+  return text//.split("\n");
 }
 
 async function main() {
   const items = await loadList();
+  const language = navigator.language;
 
   console.log(items);
 
-  doSomething(items);
+  if (items.includes(language)) {
+    const languageJSON = await getLanguageJSON(language);
+    for (let i = 0, len = languageJSON.length; i < len; i++) {
+      replaceTextEverywhere(languageJSON[i].key, languageJSON[i].value)
+    }
+  }
 }
 
-function doSomething(items) {
-  console.log(items[0]);
+
+async function getLanguageJSON(language) {
+  const response = await fetch("./" + language + ".json");
+  return await response.json();
 }
 
 main();
